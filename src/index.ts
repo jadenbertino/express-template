4b0382d6c1dcbad2a0e2d1094228eff3e25790aa
Jaden Bertino <136936125+ossaJadenBertino@users.noreply.github.com>
2024-02-22 13:27:24 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import cors from 'cors'
 import express from 'express'
 import { ENV } from './env.js'
 import handleErrors from './error.js'
-import helloWorld from './routes/hello-world.js'
 import { catchAsyncErrors, logger } from './utils.js'
 
+// Route Handlers
+import helloWorld from './routes/hello-world.js'
+
+// Express Config
 const app = express()
 app.use(cors())
 app.use(express.json())
