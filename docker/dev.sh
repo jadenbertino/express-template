538b2e0f196454c2e212856e7d7b6465ac8af415
Jaden Bertino <jaden@ossa.ai>
2024-03-22 15:41:20 -0700

    replace nodemon & ts-node with tsx

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,4 +3,4 @@ export ENVIRONMENT=staging
 # npm i -g dotenv-cli
 # https://www.npmjs.com/package/dotenv-cli
 # Avoid loading .env file from source code because that would you require you to copy the .env file into the docker image
-dotenv -e env/.env -- ts-node src/index.ts
+dotenv -e env/.env -- npx tsx src/index.ts
