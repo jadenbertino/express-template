6a19566072a08dbf91403da272e90d46b02592fd
Jaden Bertino <jaden@ossa.ai>
2024-03-22 10:33:37 -0700

    fix typo ".env."

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -5,7 +5,7 @@ set -e
 sh ./docker/utils/validate_env.sh
 . ./docker/config.sh
 # if you have multiple .env files then do .env.$ENVIRONMENT
-env_file=./env/.env.
+env_file=./env/.env
 . ./docker/utils/get_gcloud_env_args.sh $env_file
 echo "env args: $gcloud_env_args"
 
