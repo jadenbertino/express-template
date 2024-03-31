3d8952c1ab15c4192341e46f9000cf5ab13ac0a8
Jaden Bertino <jaden@ossagency.io>
2024-03-31 13:23:53 -0700

    add a check to ensure that SERVICE_NAME is set

--- docker/config.sh
+++ docker/config.sh
@@ -1,4 +1,12 @@
 . ./docker/utils/get_version.sh
 
 SERVICE_NAME=PLZ-SET-SERVICE-NAME
+
+# TODO: Delete this once SERVICE_NAME is set
+# Check if the service name has been set properly
+if [ "$SERVICE_NAME" = "PLZ-SET-SERVICE-NAME" ]; then
+    echo "Error: SERVICE_NAME is set to the default vaule. Please update it in docker/config.sh before continuing."
+    exit 1
+fi
+
 DOCKER_TAG=us-central1-docker.pkg.dev/ai-video-creation/$SERVICE_NAME/$VERSION
