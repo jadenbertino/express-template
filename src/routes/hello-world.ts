5aad432421d45da8183461284aef496b7e2445f9
Jaden Bertino <jaden@ossa.ai>
2024-05-07 13:40:15 -0700

    adjust body validation logic

--- src/routes/hello-world.ts
+++ src/routes/hello-world.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express'
 import { z } from 'zod'
-import { CustomError } from '../utils.js'
 
 const RequestSchema = z.object({ })
 
@@ -8,16 +7,17 @@ async function helloWorld(req: Request, res: Response) {
   // Validate Request
   const requestValidation = RequestSchema.safeParse(req.body)
   if (!requestValidation.success) {
-    throw new CustomError({
-      statusCode: 400,
-      message: 'Invalid request body.',
-      errors: requestValidation.error.errors,
-      received: req.body,
-    })
+    if (!requestValidation.success) {
+      return res.status(400).json({
+        message: 'Invalid request body.',
+        errors: requestValidation.error.errors,
+        received: req.body,
+      })
+    }
   }
   // const body = requestValidation.data
   
-  res.status(200).send('Hello World!')
+  return res.status(200).send('Hello World!')
 }
 
 export default helloWorld
