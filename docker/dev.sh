35db21ae3f7cc0a00be1fefebc27b8888ee2eb0b
Jaden Bertino <jaden@ossa.ai>
2024-08-02 18:08:04 -0700

    update dev script

--- docker/dev.sh
+++ docker/dev.sh
@@ -2,6 +2,8 @@ export ENVIRONMENT=staging
 
 # Load DOPPLER_TOKEN env var
 . ./env/.env.$ENVIRONMENT
+export HISTIGNORE='export DOPPLER_TOKEN*'
+export DOPPLER_TOKEN=$DOPPLER_TOKEN
 
 if [ "$1" == "--watch" ]; then
   doppler run -- npx tsx watch src/index.ts
